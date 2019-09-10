76aa4daff3ab49ddb454f9d22264565860d522c1
Petr Ospalý <petr@ospalax.cz>
2019-09-10 19:51:30 +0200

    Add script for consistent ONE Kea build
    Signed-off-by: Petr Ospalý <petr@ospalax.cz>

---
+++ extra/README.md
@@ -0,0 +1,5 @@
+# Build shortcuts
+
+Here are simple scripts to run particular builds in the consistent matter.
+
+- `onekea.sh` for OpenNebula's NFV appliance
---
+++ extra/onekea.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -ex
+
+KEA_INSTALLPREFIX=/opt/one-appliance/kea
+MAKE_JOBS=6
+IKEA_TAG=onekea
+IKEA_IMG=onekea-image
+IKEA_PKG=onekea
+KEEP_BUILDBLOB=yes
+
+export KEA_INSTALLPREFIX
+export MAKE_JOBS
+export IKEA_TAG
+export IKEA_IMG
+export IKEA_PKG
+export KEEP_BUILDBLOB
+
+WORKDIR=$(git rev-parse --show-toplevel)
+
+cd "$WORKDIR"
+
+time make package
+
+cd -
+
